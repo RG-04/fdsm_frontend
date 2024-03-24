c42cef8efe948b94fda3c1d6dab6e68839cebdfc
Parth Patil <psp90804@gmail.com>
2024-03-24 17:37:59 +0530

    fixed post dish images

--- src/helpers/PostDishImage.js
+++ src/helpers/PostDishImage.js
@@ -1,51 +1,49 @@
 import React from "react";
 
 const PostDishImage = async ({ image, uid, token }) => {
-    console.log("image", image);
-    if(image === null) {
-        return "success";
-    }
+  console.log("image", image);
+  if (image === null) {
+    return "success";
+  }
 
-    const url1 = process.env.REACT_APP_BACKEND_URL + '/api/restaurant/menu/images/' + uid;
-    console.log("hello",url1);
+  const url1 =
+    process.env.REACT_APP_BACKEND_URL + "/api/restaurant/menu/images/" + uid;
+  console.log("hello", url1);
 
-    let formData = new FormData();
-    await formData.append('image', image);
+  var formData = new FormData();
+  formData.append("image", image);
 
-    // for (var pair of formData.entries()) {
-    //     console.log(pair[0]+ ', ' + pair[1]); 
-    // }
+  // for (var pair of formData.entries()) {
+  //     console.log(pair[0]+ ', ' + pair[1]);
+  // }
 
-    console.log("formdata",formData.get('image'));
+  console.log("formdata", formData.get("image"));
 
-    try{
+  try {
     let response = await fetch(url1, {
-        method: 'POST',
-        headers: {
-            'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
-            'Authorization': `Bearer ${token}`,
-            mode: 'no-cors'
-        },
-        body: formData
-    })
-    
-    
-        if (response.ok) {
-            console.log(response);
-            const data = await response.json();
-                console.log(data);
-                return "success";
-        } else {
-            const data = await response.json();
-            console.log(data);
-            alert(data.error);
-        }
-    } catch (error) {
-        console.log(error);
-        alert('An error occurred. Please try again later.');
-    };
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
 
-    return "error";
-}
+    if (response.ok) {
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+      return "success";
+    } else {
+      const data = await response.json();
+      console.log(data);
+      alert(data.error);
+    }
+  } catch (error) {
+    console.log(error);
+    alert("An error occurred. Please try again later.");
+  }
+
+  return "error";
+};
 
 export default PostDishImage;
